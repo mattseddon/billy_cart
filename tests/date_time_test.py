95d901810da08efe81bc1489bced04011280fadc
Matt Seddon <matt.seddon@accelo.com>
2019-10-12 21:05:53 +1100

    remove unnecessary imports from test file

--- tests/date_time_test.py
+++ tests/date_time_test.py
@@ -1,9 +1,7 @@
 from tests.utils import GIVEN, WHEN, THEN
 from app.date_time import DateTime
-from datetime import datetime
 from freezegun import freeze_time
 from dateutil import tz
-import dateutil
 
 
 def test_date_helper_static():
@@ -35,43 +33,63 @@ def test_datetime_helpers():
     five_minutes_from_now = DateTime.utc_5_minutes_from_now()
     THEN("the correct string is returned")
     assert five_minutes_from_now == "1984-11-07T12:50:00Z"
-    
-    WHEN("we call is_datetime_more_than_33_seconds_from_now with a time in the distant future")
+
+    WHEN(
+        "we call is_datetime_more_than_33_seconds_from_now with a time in the distant future"
+    )
     is_future = DateTime("2019-01-01T00:00:00.000Z").is_more_than_33_seconds_from_now()
     THEN("the date time is far enough in the future to return true")
     assert is_future == True
 
-    WHEN("we call is_datetime_more_than_33_seconds_from_now with a time 34 seconds in the future")
-    is_not_future = DateTime("1984-11-07T12:45:34.000Z").is_more_than_33_seconds_from_now()
+    WHEN(
+        "we call is_datetime_more_than_33_seconds_from_now with a time 34 seconds in the future"
+    )
+    is_not_future = DateTime(
+        "1984-11-07T12:45:34.000Z"
+    ).is_more_than_33_seconds_from_now()
     THEN("the date time is far enough in the future to return true")
     assert is_not_future == True
 
-    WHEN("we call is_datetime_more_than_33_seconds_from_now with a time 33 seconds in the future")
-    is_not_future = DateTime("1984-11-07T12:44:33.000Z").is_more_than_33_seconds_from_now()
+    WHEN(
+        "we call is_datetime_more_than_33_seconds_from_now with a time 33 seconds in the future"
+    )
+    is_not_future = DateTime(
+        "1984-11-07T12:44:33.000Z"
+    ).is_more_than_33_seconds_from_now()
     THEN("the date time is not far enough in the future to return true")
     assert is_not_future == False
 
-    WHEN("we call is_datetime_more_than_33_seconds_from_now with a time that matches our override")
-    is_not_future = DateTime("1984-11-07T12:45:00.000Z").is_more_than_33_seconds_from_now()
+    WHEN(
+        "we call is_datetime_more_than_33_seconds_from_now with a time that matches our override"
+    )
+    is_not_future = DateTime(
+        "1984-11-07T12:45:00.000Z"
+    ).is_more_than_33_seconds_from_now()
     THEN("the date time is not far enough in the future to return true")
     assert is_not_future == False
 
     WHEN("we call is_datetime_more_than_33_seconds_from_now with a time from the past")
-    is_not_future = DateTime("1984-11-07T12:44:00.000Z").is_more_than_33_seconds_from_now()
+    is_not_future = DateTime(
+        "1984-11-07T12:44:00.000Z"
+    ).is_more_than_33_seconds_from_now()
     THEN("the date time is not far enough in the future to return true")
     assert is_not_future == False
+
 
 def test_date_time_str_converters():
     GIVEN("a UTC date time string with milliseconds")
     utc_date_time_str = "2019-01-01T00:00:00.000Z"
 
     WHEN("we call local_hms_45_seconds_before")
-    hour_minutes_seconds_str =  DateTime(utc_date_time_str).local_hms_45_seconds_before()
-    THEN("a string in the correct format corresponding to the local date time is returned")
+    hour_minutes_seconds_str = DateTime(utc_date_time_str).local_hms_45_seconds_before()
+    THEN(
+        "a string in the correct format corresponding to the local date time is returned"
+    )
     assert hour_minutes_seconds_str == "10:59:15"
 
     WHEN("we call utc_30_seconds_before")
-    hour_minutes_seconds_str =  DateTime(utc_date_time_str).local_hms_30_seconds_before()
-    THEN("a string in the correct format corresponding to the local date time is returned")
+    hour_minutes_seconds_str = DateTime(utc_date_time_str).local_hms_30_seconds_before()
+    THEN(
+        "a string in the correct format corresponding to the local date time is returned"
+    )
     assert hour_minutes_seconds_str == "10:59:30"
-
