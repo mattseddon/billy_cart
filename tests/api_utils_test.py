3b0901f69ff96de6afa16b4f9c300dbb1f638cd3
Matt Seddon <matt.seddon@accelo.com>
2019-10-27 19:40:29 +1100

    update api utils test not to run in offline suite

--- tests/api_utils_test.py
+++ tests/api_utils_test.py
@@ -1,11 +1,13 @@
-from tests.utils import GIVEN, WHEN, THEN
+from tests.utils import GIVEN, WHEN, THEN, should_test_real_api
 from app.third_party_adapters.api_utils import post_request
 
-def test_post_request():
-    GIVEN("a url")
-    url = 'https://httpbin.org/post'
-    WHEN("we send a post request")
-    data = post_request(url=url)
-    THEN("we get a response")
-    assert type(data) is dict
-    assert data.get("status_code") == 200
+if should_test_real_api():
+
+    def test_post_request():
+        GIVEN("a url")
+        url = "https://httpbin.org/post"
+        WHEN("we send a post request")
+        data = post_request(url=url)
+        THEN("we get a response")
+        assert type(data) is dict
+        assert data.get("status_code") == 200
